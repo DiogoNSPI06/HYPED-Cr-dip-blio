d304c24d1a199eb8231190913212a7485acc839d
Diogo Nunes Spigato Costa <50158377+DiogoNSPI06@users.noreply.github.com>
2022-10-06 22:54:03 +0000

    v1.0.2

--- routes/routing/calcule.js
+++ routes/routing/calcule.js
@@ -5,19 +5,19 @@ module.exports.Router = class Routes extends Router {
 		super();
 
 		this.get('/', function (req, res) {
-      if(req.query.type === 1) {
+      if(req.query.type === "1") {
         return res.render('Calc2.ejs', {
           title: "CrédiPublio",
           type: "Pessoal",
         })
       }
-      if(req.query.type === 2) {
+      if(req.query.type === "2") {
         return res.render('Calc2.ejs', {
           title: "CrédiPublio",
           type: "Imóveis",
         })
       }
-      if(req.query.type === 3) {
+      if(req.query.type === "3") {
         return res.render('Calc2.ejs', {
           title: "CrédiPublio",
           type: "Automóveis",
--- routes/routing/index.js
+++ routes/routing/index.js
@@ -7,6 +7,14 @@ module.exports.Router = class Routes extends Router {
 		this.get('/', function (req, res) {
 			return res.render('index.ejs', {
         title: "CrédiPublio",
+        testemonialName1: "Fernando Públio",
+        testemonialName2: "Muryllo Dionizio",
+        testemonialPhrase1: "Nossa empresa tem compromisso com seus clientes",
+        testemonialPhrase2: "Graças a PublioCrédi consegui financiar meu imóvel tão sonhado.",
+        percent: "1,24",
+        amount1: "2",
+        amount2: "5",
+        amount3: "500",
       })
 		});
 	}
